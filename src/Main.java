04eeeacc9b3c7ffcedea95cb79e7013d06ffb29a
TaisAndrade <taismdeandrade@gmail.com>
2024-10-10 10:50:22 -0300

    Update: Adiciona as classes curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
+import br.com.desafio.dominio.Curso;
+import br.com.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        }
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso de Java");
+        curso1.setCargaHoraria(40);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso C#");
+        curso2.setDescricao("Descrição do curso de C#");
+        curso2.setCargaHoraria(20);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descricao da Mentoria em Java");
+        mentoria1.setData(LocalDate.now());
+        mentoria1.setCargaHoraria(8);
+
+        System.out.println(mentoria1);
+        System.out.println(curso1.toString());
+        System.out.println(curso2.toString());
+
+
+    }
     }
